5e144252e1dca7e0cba630c9001546c5018742fc
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-06-26 23:57:16 +0100

    add database init basic code

--- db.go
+++ db.go
@@ -26,3 +26,10 @@ func NewMySQLStorage(cfg mysql.Config) *MySQLStorage {
 	log.Println("Connected to MySQL")
 	return &MySQLStorage{db: db}
 }
+
+// This needs to be created so as to initialize tables in the database
+func (s *MySQLStorage) Init() (*sql.DB, error) {
+
+	// initialize the tables
+	return s.db, nil
+}
