d85aeed749abfbab907d6ecb68c26b7417e194e3
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-06-26 23:47:20 +0100

    add database connections features

---
+++ db.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+type MySQLStorage struct {
+	db *sql.DB
+}
+
+func NewMySQLStorage(cfg mysql.Config) *MySQLStorage {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping to be sure connection succeeded
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MySQL")
+	return &MySQLStorage{db: db}
+}
